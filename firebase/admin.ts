5bb932285668a09d51114726548e25797662d381
Anshika Verma <anshikaverma.311@gmail.com>
2025-10-17 12:32:36 +0530

    Update admin.ts

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,24 +1,18 @@
-import { initializeApp, getApps, cert} from "firebase-admin/app";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import { getFirestore as getFireStore } from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
 
-const initFirebaseAdmin = () => {
-    const apps = getApps();
 
-    if (!apps.length){
-        initializeApp({
-            credential: cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        })
-    })
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-  return {
-    auth: getAuth(),
-    db: getFireStore()
-  }
-}
 
-export const {auth, db} = initFirebaseAdmin();
+export const auth = getAuth(); 
+export const db = getFirestore();
